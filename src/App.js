6fca381504bb841f4e6325f6a9b10e8006c6f19d
Justin Conger <jhconger@GMAIL.COM>
2022-06-01 16:25:14 -0500

    implemented CRUD for users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+    Outlet,
+} from "react-router-dom";
+import Navbar from './Navbar'
+import Users from './Users'
+import UserCreate from './UserCreate'
+import UserUpdate from "./UserUpdate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Users/>} />
+                    <Route path='/create' element={<UserCreate/>} />
+                    <Route exact path='/update/:id' element={<UserUpdate/>} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+
+    );
 }
-
-export default App;
